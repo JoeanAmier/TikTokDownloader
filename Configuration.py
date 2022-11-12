b0297de423255a4c3b86b889a688530e9783193e
JoeamAmier <yonglelolu@gmail.com>
2022-11-12 23:30:29 +0800

    优化配置文件

--- Configuration.py
+++ Configuration.py
@@ -6,8 +6,6 @@ class Settings:
     def __init__(self):
         self.file = "./settings.json"
         self.__default = {
-            "url": "https://v.douyin.com/XXXXXXX/",
-            "mode": "post",
             "accounts": [
                 ["https://v.douyin.com/XXXXXXX/", "post"],
             ],
@@ -20,14 +18,14 @@ class Settings:
         }
 
     def create(self):
-        with open(self.file, "w") as f:
+        with open(self.file, "w", encoding="UTF-8") as f:
             json.dump(self.__default, f)
         print("创建默认配置成功，如需修改配置，请修改“settings.json”文件后重新运行程序！")
 
     def read(self):
         try:
             if os.path.exists(self.file):
-                with open(self.file, "r") as f:
+                with open(self.file, "r", encoding="UTF-8") as f:
                     return json.load(f)
             else:
                 self.create()
@@ -36,6 +34,6 @@ class Settings:
             return {}
 
     def update(self, settings: dict):
-        with open(self.file, "w") as f:
+        with open(self.file, "w", encoding="UTF-8") as f:
             json.dump(settings, f)
         print("保存配置成功！")
