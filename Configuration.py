2915b3085983bae529e64bbf74e9612ecd28fc11
JoeamAmier <yonglelolu@gmail.com>
2023-04-09 16:40:29 +0800

    新增cookie参数

--- Configuration.py
+++ Configuration.py
@@ -15,24 +15,25 @@ class Settings:
             "time": "%Y-%m-%d %H.%M.%S",
             "split": "-",
             "music": False,
-            "save": None
+            "save": None,
+            "cookie": "",
         }  # 默认配置
 
     def create(self):
         """创建默认配置文件"""
         with open(self.file, "w", encoding="UTF-8") as f:
             json.dump(self.__default, f)
-        print("创建默认配置成功，如需修改配置，请修改“settings.json”文件后重新运行程序！")
+        print("创建默认配置成功，请修改“settings.json”文件后重新运行程序！")
 
     def read(self):
-        """读取配置文件，如果没有配置文件，则生成配置文件并返回默认配置"""
+        """读取配置文件，如果没有配置文件，则生成配置文件"""
         try:
             if os.path.exists(self.file):
                 with open(self.file, "r", encoding="UTF-8") as f:
                     return json.load(f)
             else:
                 self.create()
-                return self.__default
+                return {}  # 生成的默认配置文件必须要设置cookie才可以正常运行
         except json.decoder.JSONDecodeError:
             return {}  # 读取配置文件发生错误时返回空配置
 
