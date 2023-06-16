abf1eb899f44fca123b0ae539ea88eb9ea1d9ad1
JoeamAmier <yonglelolu@gmail.com>
2023-06-16 20:55:24 +0800

    写入Cookie信息

--- Cookie_tool.py
+++ Cookie_tool.py
@@ -6,21 +6,22 @@ class Cookie:
         self.settings = Settings()
 
     def run(self):
-        """提取 Cookie 键值对"""
+        """提取 Cookie 并写入配置文件"""
         get_key = ("passport_csrf_token", "odin_tt")
         data = {}
         cookie = input("粘贴 Cookie 后回车：")
-        for i in cookie.split('; '):
-            text = i.split("=", 1)
-            try:
-                data[text[0]] = text[1]
-            except IndexError:
-                continue
-        if result := "; ".join(f"{i}={data.get(i, '')}" for i in get_key):
-            self.write(result)
-            print(f"已写入 Cookie: {result}")
-        else:
-            print("写入 Cookie 失败！")
+        # for i in cookie.split('; '):
+        #     text = i.split("=", 1)
+        #     try:
+        #         data[text[0]] = text[1]
+        #     except IndexError:
+        #         continue
+        # if result := "; ".join(f"{i}={data.get(i, '')}" for i in get_key):
+        #     self.write(result)
+        #     print(f"已写入 Cookie: {result}")
+        # else:
+        #     print("写入 Cookie 失败！")
+        self.write(cookie)
 
     def write(self, text):
         data = self.settings.read()
