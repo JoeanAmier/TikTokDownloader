8ac1d3a8be556a4f127763e7f26bc3024d756502
JoeamAmier <yonglelolu@gmail.com>
2023-06-16 17:31:57 +0800

    提取Cookie信息

--- Cookie_tool.py
+++ Cookie_tool.py
@@ -1,13 +1,32 @@
-def main():
-    get_key = ("passport_csrf_token", "odin_tt")
-    data = {}
-    cookie = input("粘贴 Cookie 后回车：")
-    for i in cookie.split('; '):
-        text = i.split("=", 1)
-        data[text[0]] = text[1]
-    result = "; ".join(f"{i}={data.get(i, '')}" for i in get_key)
-    print(result)
+from Configuration import Settings
+
+
+class Cookie:
+    def __init__(self):
+        self.settings = Settings()
+
+    def run(self):
+        """提取 Cookie 键值对"""
+        get_key = ("passport_csrf_token", "odin_tt")
+        data = {}
+        cookie = input("粘贴 Cookie 后回车：")
+        for i in cookie.split('; '):
+            text = i.split("=", 1)
+            try:
+                data[text[0]] = text[1]
+            except IndexError:
+                continue
+        if result := "; ".join(f"{i}={data.get(i, '')}" for i in get_key):
+            self.write(result)
+            print(f"已写入 Cookie: {result}")
+        else:
+            print("写入 Cookie 失败！")
+
+    def write(self, text):
+        data = self.settings.read()
+        data["cookie"] = text
+        self.settings.update(data)
 
 
 if __name__ == '__main__':
-    main()
+    Cookie().run()
