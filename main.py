8b68babc87b70cf62748773c8057b9122e972164
JoeamAmier <yonglelolu@gmail.com>
2023-06-22 18:16:15 +0800

    项目启动入口

--- main.py
+++ main.py
@@ -1,9 +1,23 @@
+from flask import Flask
+
 from src.main_complete import TikTok
+from src.main_web_UI import webui
 
 
 def main():
-    example = TikTok()
-    example.run()
+    try:
+        mode = int(input(
+            "请输入 TikTokDownloader 运行模式: \n1. 单线程终端模式: 支持所有功能\n2. 多进程终端模式: 仅支持多账号批量下载功能\n3. Web UI 交互模式: 不支持直播下载功能\n"))
+    except ValueError:
+        return
+    match mode:
+        case 1:
+            example = TikTok()
+            example.run()
+        case 2:
+            pass
+        case 3:
+            webui(Flask(__name__)).run(host=None, debug=True)
 
 
 if __name__ == '__main__':
