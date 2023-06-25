2f115f26fe8ebfa61f70953252bfee6d7fefa18e
JoeamAmier <yonglelolu@gmail.com>
2023-06-25 23:20:16 +0800

    兼容旧版本Python

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from platform import python_version
+
 from flask import Flask
 
 from src.CookieTool import Cookie
@@ -6,25 +8,59 @@ from src.main_web_UI import WebUI
 
 
 def main():
+    """选择运行模式"""
     try:
         mode = int(
             input(
                 "请输入 TikTokDownloader 运行模式: \n0. 写入 Cookie 信息\n1. 单线程终端模式\n2. 多进程终端模式\n3. Web UI 交互模式\n"))
     except ValueError:
         return
-    # mode = 3
-    match mode:
-        case 0:
-            Cookie().run()
-        case 1:
-            example = TikTok()
-            example.run()
-        case 2:
-            pass
-        case 3:
-            """设置host=0.0.0.0可以启用局域网访问，但是本项目暂时不支持直接部署至公开服务器"""
-            app = WebUI().webui_run(Flask(__name__))
-            app.run(host=None, debug=False)
+    # mode = 3  # 调试使用
+    compatible(mode)
+
+
+def complete():
+    """单线程终端模式"""
+    example = TikTok()
+    example.run()
+
+
+def multiprocess():
+    """多进程终端模式"""
+    pass
+
+
+def web_ui():
+    """
+    Web UI 交互模式
+    设置host=0.0.0.0可以启用局域网访问
+    但是本项目暂时不支持直接部署至公开服务器
+    """
+    app = WebUI().webui_run(Flask(__name__))
+    app.run(host=None, debug=False)
+
+
+def compatible(mode: int):
+    """兼容旧版本的Python，将来可能会被移除"""
+    version = int(python_version().split(".")[1])
+    if version >= 10:
+        match mode:
+            case 0:
+                Cookie().run()
+            case 1:
+                complete()
+            case 2:
+                multiprocess()
+            case 3:
+                web_ui()
+    elif mode == 0:
+        Cookie().run()
+    elif mode == 1:
+        complete()
+    elif mode == 2:
+        multiprocess()
+    elif mode == 3:
+        web_ui()
 
 
 if __name__ == '__main__':
