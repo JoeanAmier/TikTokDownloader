85c3f7ed866b1607c1fccd259762e913b7b3b65e
JoeamAmier <yonglelolu@gmail.com>
2023-07-07 10:02:06 +0800

    更新代码

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from flask import Flask
 
 from src.CookieTool import Cookie
 from src.main_complete import TikTok
+from src.main_server import Server
 from src.main_web_UI import WebUI
 
 VERSION = "2.5 beta"
@@ -19,7 +20,7 @@ def main():
     try:
         mode = int(
             input(
-                "请输入 TikTokDownloader 运行模式: \n0. 写入 Cookie 信息\n1. 单线程终端模式\n2. 多进程终端模式\n3. Web UI 交互模式\n"))
+                "请输入 TikTokDownloader 运行模式: \n0. 写入 Cookie 信息\n1. 单线程终端模式\n2. 多进程终端模式\n3. Web UI 交互模式\n4. 服务器部署模式\n"))
     except ValueError:
         return
     compatible(mode)
@@ -39,11 +40,17 @@ def multiprocess():
 def web_ui():
     """
     Web UI 交互模式
-    设置host=0.0.0.0可以启用局域网访问
-    但是本项目暂时不支持直接部署至公开服务器
     """
     app = WebUI().webui_run(Flask(__name__))
-    app.run(host=None, debug=False)
+    app.run(host="0.0.0.0", debug=False)
+
+
+def server():
+    """
+    服务器部署模式
+    """
+    app = Server().server_run(Flask(__name__))
+    app.run(host="0.0.0.0", debug=True)
 
 
 def compatible(mode: int):
@@ -55,6 +62,8 @@ def compatible(mode: int):
         multiprocess()
     elif mode == 3:
         web_ui()
+    elif mode == 4:
+        server()
 
 
 if __name__ == '__main__':
--- src/main_server.py
+++ src/main_server.py
@@ -1,5 +1,15 @@
+from flask import render_template
+
 from src.main_complete import TikTok
 
 
 class Server(TikTok):
-    pass
+    def __init__(self):
+        super().__init__()
+
+    def server_run(self, app):
+        @app.route("/", methods=["GET"])
+        def index():
+            return render_template('server.html', )
+
+        return app
