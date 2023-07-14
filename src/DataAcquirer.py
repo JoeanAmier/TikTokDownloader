b1b67418ceb0ccf1dd8184938795587f3f65fbb0
JoeamAmier <yonglelolu@gmail.com>
2023-07-14 16:56:01 +0800

    更新随机UserAgent

--- src/DataAcquirer.py
+++ src/DataAcquirer.py
@@ -22,7 +22,7 @@ from src.StringCleaner import Cleaner
 
 def generate_user_agent():
     user_agent = [
-        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36 Edg/114.0.1823.79",
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.0.0 Safari/537.36",
     ]
 
     return choice(user_agent)
