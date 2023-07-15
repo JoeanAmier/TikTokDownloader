c4f0ace8b9f843ae6eec0823900fabb4ad5fed89
JoeamAmier <yonglelolu@gmail.com>
2023-07-15 23:19:51 +0800

    更新代码

--- src/Parameter.py
+++ src/Parameter.py
@@ -3,6 +3,7 @@ from random import randint
 from string import ascii_letters
 from string import digits
 from time import time
+from urllib.parse import urlencode
 
 from execjs import compile
 from requests import exceptions
@@ -26,8 +27,8 @@ def run_time(function):
 class XBogus:
     """代码参考: https://github.com/B1gM8c/X-Bogus/blob/main/X-Bogus.js"""
 
-    def __init__(self):
-        self.path = Path("./static/js/X-Bogus.js")
+    def __init__(self, path="./static/js/X-Bogus.js"):
+        self.path = Path(path)
         self.file = self.path.open()
         self.js = compile(self.file.read())
 
@@ -92,6 +93,44 @@ class WebID:
 
 
 if __name__ == "__main__":
+    params = {
+        "device_platform": "webapp",
+        "aid": "6383",
+        "channel": "channel_pc_web",
+        "aweme_id": "7253799364072721723",
+        "cursor": "0",
+        "count": "20",
+        "item_type": "0",
+        "insert_ids": "",
+        "rcFT": "",
+        "pc_client_type": "1",
+        "version_code": "170400",
+        "version_name": "17.4.0",
+        "cookie_enabled": "true",
+        "screen_width": "1536",
+        "screen_height": "864",
+        "browser_language": "zh-CN",
+        "browser_platform": "Win32",
+        "browser_name": "Edge",
+        "browser_version": "114.0.1823.82",
+        "browser_online": "true",
+        "engine_name": "Blink",
+        "engine_version": "114.0.0.0",
+        "os_name": "Windows",
+        "os_version": "10",
+        "cpu_core_num": "16",
+        "device_memory": "8",
+        "platform": "PC",
+        "downlink": "10",
+        "effective_type": "4g",
+        "round_trip_time": "150",
+        "webid": "7255592572578842152",
+        "msToken": "olsNApqh7VL0M3RwGRng5MPwYkPj3FttTnESDk-umJO2EC1AoT47bDf7NcDtp5AibszMtylOpE6A2q1NabOeJvYUs3XChN6yGDmleJYaPHFavf5cMRszzsvH2LjxFVmk",
+    }
+    user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36 Edg/114.0.1823.82"
+    example = XBogus("../static/js/X-Bogus.js")
+    print("X-Bogus", example.get_x_bogus(urlencode(params), user_agent))
+    example.file.close()
     print(MsToken.get_ms_token())
     print(TtWid.get_tt_wid())
-    print(WebID.get_web_id(HEADERS["User-Agent"]))
+    print("webid", WebID.get_web_id(HEADERS["User-Agent"]))
