341c5e9062a400861fb703ba8d0003e23a63b7f1
JoeamAmier <yonglelolu@gmail.com>
2023-07-27 22:40:55 +0800

    更新加密参数获取代码

--- src/Parameter.py
+++ src/Parameter.py
@@ -24,6 +24,131 @@ def run_time(function):
     return inner
 
 
+class NewXBogus:
+    string = "Dkdpgh4ZKsQB80/Mfvw36XI1R25-WUAlEi7NLboqYTOPuzmFjJnryx9HVGcaStCe="
+
+    @staticmethod
+    def disturb_array(a, b, e, d, c, f, t, n, o, i, r, _, x, u, s, l, v, h, g):
+        array = [0] * 19
+        array[0] = a
+        array[10] = b
+        array[1] = e
+        array[11] = d
+        array[2] = c
+        array[12] = f
+        array[3] = t
+        array[13] = n
+        array[4] = o
+        array[14] = i
+        array[5] = r
+        array[15] = _
+        array[6] = x
+        array[16] = u
+        array[7] = s
+        array[17] = l
+        array[8] = v
+        array[18] = h
+        array[9] = g
+        return array
+
+    @staticmethod
+    def generate_garbled_1(
+            a,
+            b,
+            e,
+            d,
+            c,
+            f,
+            t,
+            n,
+            o,
+            i,
+            r,
+            _,
+            x,
+            u,
+            s,
+            l,
+            v,
+            h,
+            g):
+        array = [0] * 19
+        array[0] = a
+        array[1] = r
+        array[2] = b
+        array[3] = _
+        array[4] = e
+        array[5] = x
+        array[6] = d
+        array[7] = u
+        array[8] = c
+        array[9] = s
+        array[10] = f
+        array[11] = l
+        array[12] = t
+        array[13] = v
+        array[14] = n
+        array[15] = h
+        array[16] = o
+        array[17] = g
+        array[18] = i
+        return "".join(map(chr, map(int, array)))
+
+    @staticmethod
+    def generate_num(text):
+        return [
+            ord(text[i]) << 16 | ord(text[i + 1]) << 8 | ord(text[i + 2]) << 0
+            for i in range(0, 21, 3)
+        ]
+
+    @staticmethod
+    def generate_garbled_2(a, b, c):
+        return chr(a) + chr(b) + c
+
+    @staticmethod
+    def generate_garbled_3(a, b):
+        d = list(range(256))
+        c = 0
+        f = ""
+        for a_idx in range(256):
+            d[a_idx] = a_idx
+        for b_idx in range(256):
+            c = (c + d[b_idx] + ord(a[b_idx % len(a)])) % 256
+            e = d[b_idx]
+            d[b_idx] = d[c]
+            d[c] = e
+        t = 0
+        c = 0
+        for b_idx in range(len(b)):
+            t = (t + 1) % 256
+            c = (c + d[t]) % 256
+            e = d[t]
+            d[t] = d[c]
+            d[c] = e
+            f += chr(ord(b[b_idx]) ^ d[(d[t] + d[c]) % 256])
+        return f
+
+    def generate_str(self, num):
+        string = [num & 16515072, num & 258048, num & 4032, num & 63]
+        string = [i >> j for i, j in zip(string, range(18, -1, -6))]
+        return "".join([self.string[i] for i in string])
+
+    def generate_x_bogus(self, query):
+        array = [64, 0.00390625, 1, 14, 15, 115, 69, 63, 86,
+                 138, 100, 193, 209, 9, 74, 226, 150, 241, 39]
+        a = self.disturb_array(*array)
+        assert a == [64, 1, 15, 69, 86, 100, 209, 74, 150, 39,
+                     0.00390625, 14, 115, 63, 138, 193, 9, 226, 241]
+        garbled = self.generate_garbled_1(*a)
+        assert garbled == "@\u0000\u0001\u000e\u000fsE?VdÁÑ\tJâñ'"
+        garbled = self.generate_garbled_3("ÿ", garbled)
+        assert garbled == "-%.* ^oÁ÷u\u0015f~~M×"
+        garbled = self.generate_garbled_2(2, 255, garbled)
+        assert garbled == "\u0002ÿ-%.* ^oÁ÷u\u0015f~~M×"
+        print("".join(self.generate_str(i)
+                      for i in self.generate_num(garbled)))
+
+
 class XBogus:
     """代码参考: https://github.com/B1gM8c/X-Bogus/blob/main/X-Bogus.js"""
 
@@ -141,3 +266,4 @@ if __name__ == "__main__":
     print(MsToken.get_ms_token())
     print(TtWid.get_tt_wid())
     print("webid", WebID.get_web_id(HEADERS["User-Agent"]))
+    NewXBogus().generate_x_bogus(None)
