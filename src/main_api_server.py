b116372601d512d2c168a903f8b1bda589023035
JoeamAmier <yonglelolu@gmail.com>
2023-08-05 19:58:44 +0800

    更新账号作品 API 接口

--- src/main_api_server.py
+++ src/main_api_server.py
@@ -5,6 +5,28 @@ from src.main_web_UI import WebUI
 
 
 class APIServer(WebUI):
+    works_keys = {
+        "Video": (
+            "作品ID",
+            "作品描述",
+            "发布时间",
+            "UID",
+            "作者昵称",
+            "账号标识",
+            "下载链接",
+            "原声信息",
+            "静态封面图",
+            "动态封面图"),
+        "Image": (
+            "作品ID",
+            "作品描述",
+            "发布时间",
+            "UID",
+            "作者昵称",
+            "账号标识",
+            "下载链接",
+            "原声信息"),
+    }
 
     def __init__(self, colour, blacklist, xb, user_agent, code, settings):
         super().__init__(colour, blacklist, xb, user_agent, code, settings)
@@ -28,6 +50,14 @@ class APIServer(WebUI):
         params["root"] = root
         params["params"] = params_
 
+    def format_data(self, data: list, type_: str):
+        result = []
+        keys = self.works_keys[type_]
+        for item in data:
+            dict_ = {keys[i]: j for i, j in enumerate(item)}
+            result.append(dict_)
+        return result
+
     def run_server(self, app):
         @app.route('/init/', methods=['POST'])
         def init():
@@ -47,7 +77,6 @@ class APIServer(WebUI):
                 "mode": request.json.get('mode', "post"),
                 "earliest": request.json.get('earliest', ""),
                 "latest": request.json.get('latest', ""),
-                "api": True,
             }
             self.manager = Cache(
                 self.logger,
@@ -56,10 +85,11 @@ class APIServer(WebUI):
                 mark=self.mark,
                 name=self.nickname)
             self.add_params(params)
-            video, image = self.get_account_works(**params)
+            self.download.download = False
+            self.get_account_works(**params)
             return {
-                "video": video,
-                "image": image,
+                "video": self.format_data(self.download.video_data, "Video"),
+                "image": self.format_data(self.download.image_data, "Image"),
                 "message": "success",
             }
 
