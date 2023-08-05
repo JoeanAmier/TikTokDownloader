65746f5d98be9a518ca59914438a94ef09ff2776
JoeamAmier <yonglelolu@gmail.com>
2023-08-05 17:07:15 +0800

    更新 Web API 接口模式

--- src/main_api_server.py
+++ src/main_api_server.py
@@ -1,5 +1,34 @@
+from flask import request
+
 from src.main_web_UI import WebUI
 
 
 class APIServer(WebUI):
-    pass
+
+    def __init__(self, colour, blacklist, xb, user_agent, code, settings):
+        super().__init__(colour, blacklist, xb, user_agent, code, settings)
+
+    def update_parameters(self, parameters):
+        parameters = {
+            i: j for i,
+            j in parameters.items() if i in self.settings_keys}
+        settings = self.settings.read()
+        for i, j in parameters.items():
+            settings[i] = j
+        self.settings.update(settings)
+        if c := parameters.get("cookie"):
+            self.cookie.extract(c, 0)
+        self.configuration(filename=self.filename)
+
+    def run_server(self, app):
+        @app.route('/init/', methods=['POST'])
+        def init():
+            print(request.json)
+            self.update_parameters(request.json)
+            return {"message": "success"}
+
+        @app.route('/user/', methods=['POST'])
+        def user():
+            print(request.form)
+
+        return app
