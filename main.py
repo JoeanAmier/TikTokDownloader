ee72fa6385cc0124a635de154c941dda6c3b5330
JoeamAmier <yonglelolu@gmail.com>
2023-08-23 19:19:00 +0800

    更新免责声明

--- main.py
+++ main.py
@@ -36,6 +36,21 @@ class TikTokDownloader:
     UPDATE = {"path": Path("./src/config/Disable_Update")}
     COLOUR = {"path": Path("./src/config/Disable_Colour")}
     RECORD = {"path": Path("./src/config/Disable_Record")}
+    DISCLAIMER = {"path": Path("./src/config/Consent_Disclaimer")}
+
+    DISCLAIMER_TEXT = (
+        "关于 TikTokDownloader 的 免责声明：",
+        "",
+        "1. 使用者对本项目的使用由使用者自行决定，并自行承担风险。作者对使用者使用本项目所产生的任何损失、责任、或风险概不负责。",
+        "2. 本项目的作者提供的代码和功能是基于现有知识和技术的开发成果。作者尽力确保代码的正确性和安全性，但不保证代码完全没有错误或缺陷。",
+        "3. 使用者在使用本项目时必须严格遵守 GNU General Public License v3.0 的要求，并在适当的地方注明使用了 GNU General Public License v3.0 的代码。",
+        "4. 使用者在任何情况下均不得将本项目的作者、贡献者或其他相关方与使用者的使用行为联系起来，或要求其对使用者使用本项目所产生的任何损失或损害负责。",
+        "5. 使用者在使用本项目的代码和功能时，必须自行研究相关法律法规，并确保其使用行为合法合规。任何因违反法律法规而导致的法律责任和风险，均由使用者自行承担。",
+        "",
+        "在使用本项目的代码和功能之前，请您认真考虑并接受以上免责声明。如果您对上述声明有任何疑问或不同意，请不要使用本项目的代码和功能。如果"
+        "您使用了本项目的代码和功能，则视为您已完全理解并接受上述免责声明，并自愿承担使用本项目的一切风险和后果。",
+        "",
+    )
 
     def __init__(self):
         self.colour = None
@@ -50,6 +65,17 @@ class TikTokDownloader:
             self.x_bogus,
             self.user_agent,
             self.code)
+
+    def disclaimer(self):
+        if not self.DISCLAIMER["path"].exists():
+            print(self.colour.colorize("\n".join(self.DISCLAIMER_TEXT), 93))
+            if input(
+                    self.colour.colorize(
+                        "是否已仔细阅读上述免责声明(YES/NO)",
+                        93)).upper() != "YES":
+                exit()
+            deal_config(self.DISCLAIMER["path"])
+            print()
 
     def version(self):
         print(
@@ -198,6 +224,7 @@ class TikTokDownloader:
         self.version()
         self.check_update()
         self.check_settings()
+        self.disclaimer()
         self.main()
         self.delete_temp()
 
