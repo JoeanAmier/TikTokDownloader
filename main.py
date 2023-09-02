7abd5b6ee2fbae6582ed65e3b0b883745b39946c
JoeamAmier <yonglelolu@gmail.com>
2023-09-02 20:04:52 +0800

    发布 4.1 版本

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ from src.main_web_UI import WebUI
 
 class TikTokDownloader:
     VERSION = 4.1
-    STABLE = False
+    STABLE = True
 
     REPOSITORY = "https://github.com/JoeanAmier/TikTokDownloader"
     LICENCE = "GNU General Public License v3.0"
