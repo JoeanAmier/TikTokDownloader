05cef6e24ca6d5087032bc598df8a085284a4644
JoeamAmier <yonglelolu@gmail.com>
2023-09-09 16:36:01 +0800

    重构项目代码

--- src/DataExtractor.py
+++ src/DataExtractor.py
@@ -1,3 +1,7 @@
+from datetime import datetime
+from time import localtime
+from time import strftime
+
 from src.DataAcquirer import Parameter
 
 __all__ = ["Extractor"]
@@ -6,6 +10,7 @@ __all__ = ["Extractor"]
 class Extractor:
     def __init__(self, params: Parameter):
         self.log = params.log
+        self.time = params.time
         self.type = {
             "user": self.user,
             "works": self.works,
@@ -23,28 +28,72 @@ class Extractor:
         except KeyError:
             return ""
 
-    def run(self, data: list[dict], type_="works", **kwargs) -> list[dict]:
+    def run(self, data: list[dict], type_="works", **kwargs) -> dict:
         if type_ not in self.type.keys():
             raise ValueError
         return self.type[type_](data, **kwargs)
 
-    def user(self, data: list[dict]) -> list[dict]:
+    def user(self, data: list[dict], post=True) -> list[dict]:
+        result = []
+        if post:
+            [self.extract_user(result, item) for item in data]
+        else:
+            [self.extract_user(result, item) for item in data[:-1]]
+            self.extract_not_post(result, data[-1])
+        return result
+
+    def extract_user(self, container: list, data: dict) -> None:
+        data_dict = {
+            "collection_time": datetime.now().strftime("%Y-%m-%d %H:%M:%S")}
+        self.extract_works_info(data_dict, data)
+        self.extract_account_info(data_dict, data)
+        container.append(data_dict)
+
+    @staticmethod
+    def extract_description(data: dict) -> str:
+        try:
+            desc = data["share_info"]["share_link_desc"]
+            return desc.split(
+                ":/ ", 1)[-1].rstrip("  %s 复制此链接，打开Dou音搜索，直接观看视频！")
+        except (KeyError, IndexError):
+            return ""
+
+    def format_date(self, data: dict) -> str:
+        return strftime(
+            self.time,
+            localtime(
+                data["create_time"]))
+
+    def extract_works_info(self, item: dict, data: dict) -> None:
+        item["id"] = data["aweme_id"]
+        item["desc"] = self.extract_description(data)
+        item["create_time"] = self.format_date(data)
+
+    def extract_account_info(self, item: dict, data: dict) -> None:
         pass
 
-    def works(self, data: list[dict]) -> list[dict]:
+    @staticmethod
+    def extract_not_post(container: list, data: dict) -> None:
+        data_dict = {
+            "nickname": data["author"]["nickname"],
+            "uid": data["author"]["uid"],
+        }
+        container.append(data_dict)
+
+    def works(self, data: list[dict]) -> dict:
         pass
 
-    def comment(self, data: list[dict]) -> list[dict]:
+    def comment(self, data: list[dict]) -> dict:
         pass
 
-    def live(self, data: list[dict]) -> list[dict]:
+    def live(self, data: list[dict]) -> dict:
         pass
 
-    def search_general(self, data: list[dict]) -> list[dict]:
+    def search_general(self, data: list[dict]) -> dict:
         pass
 
-    def search_user(self, data: list[dict]) -> list[dict]:
+    def search_user(self, data: list[dict]) -> dict:
         pass
 
-    def hot(self, data: list[list[dict]]) -> list[list[dict]]:
+    def hot(self, data: list[list[dict]]) -> dict:
         pass
