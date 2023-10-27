b1660b1eb1a4e7dc78f56e66d1e634f586aaaf8e
JoeamAmier <yonglelolu@gmail.com>
2023-10-27 21:17:43 +0800

    重构项目代码

--- src/Customizer.py
+++ src/Customizer.py
@@ -1,4 +1,3 @@
-# 启用延迟需要取消注释
 # from random import randint
 # from time import sleep
 
@@ -20,6 +19,7 @@ __all__ = [
     "INFO",
     "GENERAL",
     "PROGRESS",
+    "rest"
 ]
 
 # 线程池最大线程数量，多线程下载文件时使用
@@ -52,7 +52,10 @@ INFO = "b bright_green"
 
 
 def wait():
-    """设置网络请求间隔时间，仅对获取数据生效，不影响下载文件"""
+    """
+    设置网络请求间隔时间，仅对获取数据生效，不影响下载文件
+    启用延迟需要将第 1 ~ 2 行代码取消注释
+    """
     # 随机延时
     # sleep(randint(15, 35) * 0.1)
     # 固定延时
@@ -90,3 +93,23 @@ def check_login():
 def conditional_filtering(data: list[dict]) -> list[dict]:
     """自定义作品筛选规则，例如：筛选作品点赞数、作品类型、视频分辨率等"""
     return data
+
+
+def rest(count: int, screen):
+    """
+    如需采集大量账号数据，请启用该函数，可以在处理指定数量的账号后，暂停一段时间，然后继续运行
+    batches: 每次处理的账号数量上限，比如：每次处理 10 个账号，就会暂停程序
+    rest_time: 程序暂停的时间，单位：秒；比如：每处理 10 个账号，就暂停 10 分钟
+    启用该函数需要将第 2 行代码取消注释
+    仅对终端命令行模式的批量下载账号作品模式生效
+    """
+    # 启用该函数
+    # batches = 10  # 根据实际需求修改
+    # if not count % batches:
+    #     rest_time = 60 * 10  # 根据实际需求修改
+    #     screen(
+    #         f"程序已经处理了 {batches} 个账号，为了避免请求频率过高导致账号或 IP 被风控，程序已经暂停，"
+    #         f"在 {rest_time} 秒后继续处理账号！", style=GENERAL)
+    #     sleep(rest_time)
+    # 默认不启用
+    pass
