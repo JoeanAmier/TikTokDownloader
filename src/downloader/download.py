c174271f0eed5614e90fd5330c13b96ef5d7d02e
Son Pham <sp@outlook.com.vn>
2024-07-16 17:32:17 +0700

    modified user folder name

--- src/downloader/download.py
+++ src/downloader/download.py
@@ -1,6 +1,7 @@
 from asyncio import Semaphore
 from asyncio import gather
 from datetime import datetime
+import os
 from pathlib import Path
 from shutil import move
 from time import time
@@ -516,13 +517,13 @@ class Downloader:
             mix=False,
             folder_name: str = None) -> Path:
         if batch and all((id_, name, addition)):
-            folder_name = f"UID{id_}_{mark or name}_{addition}"
+            folder_name = f"{folder_name or self.folder_name}/{mark or name}/videos/"#f"UID{id_}_{mark or name}_{addition}"
         elif mix and all((id_, name, addition)):
-            folder_name = f"MIX{id_}_{mark or name}_{addition}"
+            folder_name = f"{folder_name or self.folder_name}/{mark or name}/mixes/"#f"MIX{id_}_{mark or name}_{addition}"
         else:
             folder_name = folder_name or self.folder_name
         folder = self.root.joinpath(folder_name)
-        folder.mkdir(exist_ok=True)
+        folder.mkdir(exist_ok=True, parents=True)
         return folder
 
     def generate_detail_name(self, data: dict) -> str:
