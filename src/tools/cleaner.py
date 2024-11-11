8a23250b2594f3c39de06320fc8579f453fef30a
JoeanAmier <yonglelolu@gmail.com>
2024-11-11 19:10:07 +0800

    perf(cleaner.py): 优化文件名称非法字符处理

--- src/tools/cleaner.py
+++ src/tools/cleaner.py
@@ -1,14 +1,17 @@
 from platform import system
+from re import compile
 from string import whitespace
 
 from emoji import replace_emoji
 
-from ..custom import illegal_nickname
+from src.custom import illegal_nickname
 
 __all__ = ["Cleaner"]
 
 
 class Cleaner:
+    CONTROL_CHARACTERS = compile(r"[\x00-\x1F\x7F]")
+
     def __init__(self):
         """
         替换字符串中包含的非法字符，默认根据系统类型生成对应的非法字符字典，也可以自行设置非法字符字典
@@ -70,6 +73,8 @@ class Cleaner:
         """过滤文件夹名称中的非法字符"""
         text = text.replace(":", ".")
 
+        text = self.remove_control_characters(text)
+
         text = self.filter(text)
 
         text = replace_emoji(text)
@@ -89,8 +94,14 @@ class Cleaner:
         """将连续的空格转换为单个空格"""
         return " ".join(string.split())
 
+    @classmethod
+    def remove_control_characters(cls, text, replace="", ):
+        # 使用正则表达式匹配所有控制字符
+        return cls.CONTROL_CHARACTERS.sub(replace, text, )
+
 
 if __name__ == "__main__":
     demo = Cleaner()
     print(demo.rule)
     print(demo.filter_name(""))
+    print(demo.remove_control_characters("hello \x08world"))
