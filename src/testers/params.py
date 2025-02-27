f12142fbd27ee2fd461a27cc3b96b41e57293f3f
全 <yonglelolu@gmail.com>
2025-02-27 20:02:36 +0800

    feat(params): 添加 Cookie 配置文件读取功能
    - 新增 ConfigParser 用于读取配置文件
    - 在 Params 类中添加 create_ini 和 read_ini 方法
    - 更新 Params 类的构造函数，初始化时读取配置文件
    - 添加测试函数以验证配置文件读取功能
    - 更新 .gitignore 文件，排除 .ini 文件

--- src/testers/params.py
+++ src/testers/params.py
@@ -1,3 +1,5 @@
+from configparser import ConfigParser, NoSectionError, NoOptionError
+
 from rich.console import Console
 
 from src.custom import (
@@ -13,8 +15,8 @@ from src.tools import create_client
 
 class Params:
     def __init__(self):
-        self.cookie_str = "自行填入抖音 Cookie"
-        self.cookie_str_tiktok = "自行填入 TikTok Cookie"
+        self.cookie_str = ""
+        self.cookie_str_tiktok = ""
         self.headers = DATA_HEADERS | {"Cookie": self.cookie_str}
         self.headers_tiktok = DATA_HEADERS_TIKTOK | {
             "Cookie": self.cookie_str_tiktok,
@@ -34,6 +36,32 @@ class Params:
             timeout=self.timeout,
             proxy="http://127.0.0.1:10809",
         )
+        self.config = ConfigParser()
+        self.read_ini()
+
+    def create_ini(self):
+        self.config["Cookie"] = {
+            "dy": "",
+            "tk": "",
+        }
+        with open("cookie.ini", "w", encoding="utf-8") as configfile:
+            self.config.write(configfile)
+
+    def read_ini(self):
+        if not self.config.read("cookie.ini"):
+            self.create_ini()
+            return
+        try:
+            self.cookie_str = self.config.get(
+                "Cookie",
+                "dy",
+            )
+            self.cookie_str_tiktok = self.config.get(
+                "Cookie",
+                "tk",
+            )
+        except (NoSectionError, NoOptionError) as e:
+            print(f"读取 Cookie 错误: {e}")
 
     async def __aenter__(self):
         return self
@@ -41,3 +69,15 @@ class Params:
     async def __aexit__(self, exc_type, exc_val, exc_tb):
         await self.client.aclose()
         await self.client_tiktok.aclose()
+
+
+async def test():
+    async with Params() as params:
+        print(params.cookie_str)
+        print(params.cookie_str_tiktok)
+
+
+if __name__ == "__main__":
+    from asyncio import run
+
+    run(test())
