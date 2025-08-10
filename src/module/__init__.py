4ff3f71a32748e3b1c39257c150ad962622c762a
Quan <yonglelolu@foxmail.com>
2025-08-10 21:19:53 +0800

    perf: 禁用依赖 qrcode 库的代码

--- src/module/__init__.py
+++ src/module/__init__.py
@@ -1,13 +1,14 @@
 from .cookie import Cookie
 from .ffmpeg import FFMPEG
 from .migrate_folder import MigrateFolder
-from .register import __Register
+
+# from .register import __Register
 from .tiktok_unofficial import DetailTikTokExtractor, DetailTikTokUnofficial
 
 __all__ = [
     "Cookie",
     "FFMPEG",
-    "__Register",
+    # "__Register",
     "DetailTikTokExtractor",
     "DetailTikTokUnofficial",
     "MigrateFolder",
